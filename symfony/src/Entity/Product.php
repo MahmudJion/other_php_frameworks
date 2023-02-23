814fd43ec459744b3185ec5751579f4258f58687
MahmudJion <mahmudh.jion02@gmail.com>
2023-02-23 19:58:17 +0000

    update Product.php

--- symfony/src/Entity/Product.php
+++ symfony/src/Entity/Product.php
@@ -1,5 +1,4 @@
 <?php
-// src/Entity/Product.php
 
 namespace App\Entity;
 
@@ -19,7 +18,7 @@ class Product
     private $id;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=255)
      */
     private $name;
 
@@ -29,9 +28,65 @@ class Product
     private $description;
 
     /**
-     * @ORM\Column(type="decimal", scale=2)
+     * @ORM\Column(type="decimal", precision=10, scale=2)
      */
     private $price;
 
-    // ... getters and setters
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stock;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
 }
