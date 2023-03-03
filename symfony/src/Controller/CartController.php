e25bdb52fee952ba5b17dc08c874576866f51c0d
MahmudJion <mahmudh.jion02@gmail.com>
2023-03-03 18:39:32 +0000

    add function on CartController

--- symfony/src/Controller/CartController.php
+++ symfony/src/Controller/CartController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Service\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="app_cart")
+     * @Route("/cart/add/{id}", name="cart_add", methods={"POST"})
      */
-    public function index(): Response
+    public function add(Request $request, Product $product, Cart $cart): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $quantity = $request->request->get('quantity', 1);
+        $cart->addProduct($product, $quantity);
+
+        $this->addFlash('success', sprintf('%d x %s added to cart', $quantity, $product->getName()));
+
+        return $this->redirectToRoute('product_details', ['id' => $product->getId()]);
     }
 }
