3816302cb2a8f16110967aac23ab23c7fb8ac39b
MahmudJion <mahmudh.jion02@gmail.com>
2023-03-07 12:24:48 +0000

    update CheckoutController.php

--- symfony/src/Controller/CheckoutController.php
+++ symfony/src/Controller/CheckoutController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\Checkout;
+use App\Entity\Order;
+use App\Form\CheckoutType;
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CheckoutController extends AbstractController
 {
     /**
-     * @Route("/checkout", name="app_checkout")
+     * @Route("/checkout", name="checkout")
      */
-    public function index(): Response
+    public function checkout(Request $request, CartService $cartService)
     {
-        return $this->render('checkout/index.html.twig', [
-            'controller_name' => 'CheckoutController',
+        $checkout = new Checkout();
+
+        $form = $this->createForm(CheckoutType::class, $checkout);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Process the payment and complete the order
+            $cart = $cartService->getCart();
+            $order = $cartService->checkout($cart, $checkout);
+
+            return $this->redirectToRoute('order_confirmation', [
+                'orderId' => $order->getId(),
+            ]);
+        }
+
+        return $this->render('checkout.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
